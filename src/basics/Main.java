64fba54f26a462cebdb0fcf4ea466aaf1a1195d7
Milan <milan.stojadinovic.77@gmail.com>
2021-07-26 12:58:39 +0200

    Initial commit

---
+++ src/basics/Main.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Integer[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		
+		System.out.println(numbers);
+
+	}
+
+}
